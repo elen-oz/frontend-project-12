4025dcf6947ac3febdbbf7827675223332f38a27
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-19 23:10:38 +0200

    fix errors

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/elen-oz/frontend-project-12/workflows/hexlet-check/badge.svg)](https://github.com/elen-oz/frontend-project-12/actions)
 
 ### [Chat](https://frontend-project-12-production-4936.up.railway.app)
+
+<!-- https://frontend-project-12-blush.vercel.app/ -->
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-quotes */
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable import/no-extraneous-dependencies */
@@ -7,17 +8,27 @@ import NotFoundPage from './pages/notFoundPage';
 import Nav from './components/Nav';
 
 const App = () => (
-  <div className="h-100 bg-light">
-    <div className="h-100">
-      <div className="h-100" id="chat">
-        <div className="d-flex flex-column h-100">
+  <div className='h-100 bg-light'>
+    <div className='h-100'>
+      <div
+        className='h-100'
+        id='chat'
+      >
+        <div className='d-flex flex-column h-100'>
           <Nav />
           <BrowserRouter>
             <Routes>
               {['/', '/login'].map((path, index) => (
-                <Route path={path} element={<Login />} key={index} />
+                <Route
+                  path={path}
+                  element={<Login />}
+                  key={index}
+                />
               ))}
-              <Route path="*" element={<NotFoundPage />} />
+              <Route
+                path='*'
+                element={<NotFoundPage />}
+              />
             </Routes>
           </BrowserRouter>
         </div>
